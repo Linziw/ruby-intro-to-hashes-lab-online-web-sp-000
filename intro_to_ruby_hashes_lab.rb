f1a79bc49955ac6b60258496f05254e5db647071
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-26 10:48:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,9 @@ end
 def monopoly_with_second_tier
 	monopoly = {railroads: {} }
 	monopoly[:railroads][:pieces] = 4
+	monopoly[:railroads][:names] = {}
+	monopoly[:railroads][:rent_in_dollars] = {}
 	monopoly
-
 end
 
 def monopoly_with_third_tier
